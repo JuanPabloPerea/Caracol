acdb06ba044efa9427483f1b0bea5a5c369e1c71
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-06-25 13:05:55 -0500

    Update laberinto.py

--- laberinto.py
+++ laberinto.py
@@ -1,11 +1,9 @@
 def mapa():
     return [x.split(" ") for x in open("prueba.txt").readlines()]
 
-"""
 # lista de listas sin el \n
 def nuevomapa():
     return [[x.replace('\n','') for x in caminos] for caminos in mapa()]
-"""
 
 #busca la posicion de x en la matriz
 def buscar_en_matriz(matriz,cont):
