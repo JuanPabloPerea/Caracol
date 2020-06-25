0d5d5f2c141f636303467e48df8b055c6305e25d
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-06-25 13:05:36 -0500

    Update laberinto.py

--- laberinto.py
+++ laberinto.py
@@ -15,30 +15,4 @@ def buscar_en_matriz(matriz,cont):
         return (cont,matriz[0].index("X"))
     return buscar_en_matriz(matriz[1:],cont+1)
 #Ejemplo de movimiento
-"""
-#hace un movimiento a la derecha siempre y cuando
-#aun tenga espacio
-def der(mapa,pos):
-    return [['0' if i==pos[0] and j== pos[1]
-             else 'x' if i== pos[0] and j== pos[1]+1
-             else mapa[i][j]  for j in range(len(mapa[i])) ]  for i in range(len(mapa)) ]
-def aba(mapa,pos):
-    return [['0' if i==pos[0] and j== pos[1]
-             else 'x' if i== pos[0]+1 and j== pos[1]
-             else mapa[i][j]  for j in range(len(mapa[i])) ]  for i in range(len(mapa)) ]
-
-def izq(mapa,pos):
-    return [['0' if i==pos[0] and j== pos[1]
-             else 'x' if i== pos[0] and j== pos[1]-1
-             else mapa[i][j]  for j in range(len(mapa[i])) ]  for i in range(len(mapa)) ]
-
-def arr(mapa,pos):
-    return [['0' if i==pos[0] and j== pos[1]
-             else 'x' if i== pos[0]-1 and j== pos[1]
-             else mapa[i][j]  for j in range(len(mapa[i])) ]  for i in range(len(mapa)) ]
-             
-#print(arr(mapa(),buscar_en_matriz(mapa(),0)))
-
-#def solucion(mapa,pos):
-""" 
 
